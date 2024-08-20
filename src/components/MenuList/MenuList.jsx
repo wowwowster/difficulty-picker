65da16e8a9ff1eb53a58edf9302151bb74ca7b75
wowwowster <wowwowster@gmail.com>
2024-08-20 15:42:59 +0200

    Fin chapitre Utilisation de map

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,29 +1,19 @@
+import { MenuListItem } from "../MenuListItem/MenuListItem";
 import s from "./style.module.css";
-import { MenuListItem } from './../MenuListItem/MenuListItem';
+import { DIFFICULTIES } from "./constant";
 
 export function MenuList({ difficulty, onItemClick }) {
   return (
-    <div  className={s.container} >
-      <MenuListItem 
-        onClick={onItemClick} 
-        difficulty="Low" 
-        isSelected={difficulty === "Low"} 
-      />
-      <MenuListItem 
-        onClick={onItemClick} 
-        difficulty="Medium" 
-        isSelected={difficulty === "Medium"} 
-      />
-      <MenuListItem 
-        onClick={onItemClick} 
-        difficulty="High" 
-        isSelected={difficulty === "High"} 
-      />
-      <MenuListItem 
-        onClick={onItemClick} 
-        difficulty="Insane" 
-        isSelected={difficulty === "Insane"} 
-      />
+    <div className={s.container}>
+      {DIFFICULTIES.map((diff) => {
+        return (
+          <MenuListItem
+            onClick={onItemClick}
+            difficulty={diff}
+            isSelected={difficulty === diff}
+          />
+        );
+      })}
     </div>
   );
 }
